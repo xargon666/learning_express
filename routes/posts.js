b883223f79c7a2f836252ecf7bdf13676c883213
Chris Pucknell <chris.pucknell@gmail.com>
2024-05-17 17:06:16 +0100

    wip

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,5 @@
 
-const express = require('express')
+import express from 'express'
 const router = express.Router()
 // post data (would normally be in a database or something)
 let posts = [
@@ -7,13 +7,19 @@ let posts = [
     { id: 2, title: 'Post Two' },
     { id: 3, title: 'Post Three' }
 ]
+// Missing Post msg
+const missingPost = (id) => {
+    if (isNaN(id)) return { msg: 'Please search for a valid post id number' }
+    return { msg: `A post with the id of ${id} was not found.` }
+}
+
 // Get all posts
 router.get('/', (req, res) => {
     const limit = parseInt(req.query.limit)
     if (!isNaN(limit) && limit > 0) {
         return res
-        .status(200)
-        .json(posts.slice(0, limit))
+            .status(200)
+            .json(posts.slice(0, limit))
     }
 
     res.status(200).json(posts)
@@ -25,11 +31,61 @@ router.get('/:id', (req, res) => {
     const post = posts.find((post) => post.id === id)
     if (!post) {
         return res
-        .status(404)
-        .json({ msg: `A post with the id of ${id} was not found.` })
+            .status(404)
+            .json(missingPost(id))
     }
 
     res.status(200).json(post)
 })
 
-module.exports = router
+// Create a new post
+router.post('/', (req, res) => {
+    console.log(req.body)
+    const newPostId = posts.length + 1
+    const newPost = {
+        id: newPostId,
+        title: req.body.title
+    }
+    if (!newPost.title) {
+        return res
+            .status(400)
+            .json({ msg: 'Please include a title' })
+    }
+    posts.push(newPost)
+    res
+    .status(201).json(posts)
+})
+
+
+// Update a post
+router.put('/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const post = posts.find((post) => post.id === id)
+
+    if (!post) {
+        return res
+            .status(404)
+            .json(missingPost(id))
+    }
+
+    post.title = req.body.title
+    res.status(200).json(posts)
+})
+
+// Delete Post
+
+router.delete('/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const post = posts.find((post) => post.id === id)
+
+    if (!post) {
+        return res
+            .status(404)
+            .json(missingPost(id))
+    }
+
+    posts.filter((post) => post.id === id)
+    res.status(200).json(posts)
+})
+
+export default router
--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 import express from 'express'
 import path from 'path'
-import posts from './roues/posts'
+import posts from './routes/posts.js'
 const SERVER_PORT = process.env.SERVER_PORT || 8000
 
 const app = express()
 // setup static folder
 // app.use(express.static(path.join(__dirname,'public')))
 
+// Body parse middleware
+app.use(express.json()) // for raw json
+app.use(express.urlencoded({extended:false})) // for encoded data
+
 // Routes
 app.use('/api/posts',posts)
 
-
 app.listen(SERVER_PORT, () => console.log(`Server is running on port http://localhost:${SERVER_PORT}`))
+ 
